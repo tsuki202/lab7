a76bc9a8016f0ed4c25a01ecf63e77c3d8c62991
Solodka Inna <104030836+tsuki202@users.noreply.github.com>
2024-12-03 11:35:47 +0200

    Delete src directory

--- src/task1.java
+++
@@ -1,8 +0,0 @@
-import java.util.Random;
-public class task1 {
-    public static void main(String[] args) {
-        Random random = new Random();
-        int randomNumber = random.nextInt(41) - 20; // Генеруємо випадкове число від -20 до 20
-        System.out.println("Випадкове натуральне число з проміжку [-20; 20]: " + randomNumber);
-    }
-}
